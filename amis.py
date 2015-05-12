6ce4bcafea38bf325775af2f722f370a486815d4
Mark Muetzelfeldt <markmuetz@gmail.com>
2015-05-12 12:47:43 +0100

    Updated AMIs.

--- amis.py
+++ amis.py
@@ -7,6 +7,8 @@ UBUNTU_1404_AMD64_AMI = 'ami-accff2b1'
 ST_WORKER_IMAGE_1 = 'ami-4acff057'
 # Built after modifying ST_WORKER_IMAGE_1 - changing file structure.
 ST_WORKER_IMAGE_2 = 'ami-06ad921b'
+# Built after modifying ST_WORKER_IMAGE_2 - deleting 2003's data
+ST_WORKER_IMAGE_3 = 'ami-38704e25'
 # Convenient for using as default.
-ST_WORKER_IMAGE_CURRENT = ST_WORKER_IMAGE_2
-ST_WORKER_IMAGE_NAMETAG = 'st_worker_image_2'
+ST_WORKER_IMAGE_CURRENT = ST_WORKER_IMAGE_3
+ST_WORKER_IMAGE_NAMETAG = 'st_worker_image_3'
